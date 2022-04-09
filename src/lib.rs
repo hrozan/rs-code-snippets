e63da8c58da5b974e5763d52dacb2f5bd3223645
hrozan <higorb.rozan@gmail.com>
2022-04-09 02:04:34 -0300

    feat: add selection sort

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,56 @@
-pub fn bubble_sort(arr: &mut Vec<i32>) {
-    let mut has_swap = true;
-    while has_swap {
-        has_swap = false;
-        for i in 0..arr.len() - 1 {
-            if arr[i] > arr[i + 1] {
-                let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                has_swap = true;
+fn swap(arr: &mut Vec<i32>, i: usize, t: usize) {
+    let temp = arr[i];
+    arr[i] = arr[t];
+    arr[t] = temp;
+}
+
+#[allow(dead_code)]
+fn bubble_sort(arr: &mut Vec<i32>) {
+    let mut has_swapped = true;
+    while has_swapped {
+        has_swapped = false;
+        for i in 1..arr.len() {
+            if arr[i - 1] > arr[i] {
+                swap(arr, i - 1, i);
+                has_swapped = true
             }
         }
     }
 }
 
+#[allow(dead_code)]
+fn selection_sort(arr: &mut Vec<i32>) {
+    let mut min_idx: usize;
+    for i in 0..arr.len() - 1 {
+        min_idx = i;
+        for j in i + 1..arr.len() {
+            if arr[j] < arr[min_idx] {
+                min_idx = j
+            }
+        }
+
+        swap(arr, min_idx, i)
+    }
+}
+
+#[test]
+fn test_selection_sort() {
+    let mut arr = vec![5, 2, 8, 3, 9];
+
+    selection_sort(&mut arr);
+
+    for i in 0..arr.len() - 1 {
+        assert!(arr[i] <= arr[i + 1])
+    }
+}
+
 #[test]
 fn test_bubble_sort() {
-    let mut arr: Vec<i32>  = vec![8, 3, 5, 2, 7, 1];
+    let mut arr = vec![5, 3, 2, 6, 4, 78];
 
     bubble_sort(&mut arr);
 
     for i in 0..arr.len() - 1 {
-        assert!(arr[i] <= arr[i + 1]);
+        assert!(arr[i] <= arr[i + 1])
     }
 }
